1e73109cf97ddba848f39990671888ebf95b5839
Gustavo Costa <gucostarp@gmail.com>
2021-05-29 17:16:16 -0300

    Projeto

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'amazon-web-services::us-east-1',
+    username: 'rehrtjxb',
+    password: 'sZnVWu-c-3ZFqcpuhgcJaa274QbGGIVE ',
+    database: 'rehrtjxb',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }
+}
